53582554506196372357d44a5d3054f0e629d07d
D_Zenith <dlw2002@outlook.com>
2022-03-07 16:54:02 +0100

    smooth camera movement + physics fps adjustment

--- player/scripts/cam_controls.gd
+++ player/scripts/cam_controls.gd
@@ -1,48 +1,28 @@
 extends Spatial
 
-#camera variables
-const CAMERA_MOUSE_ROTATION_SPEED = 0.002
-const CAMERA_CONTROLLER_ROTATION_SPEED = 2.8
-const CAMERA_X_ROT_MIN = -40
-const CAMERA_X_ROT_MAX = 70
+var camrot_h = 0
+var camrot_v = 0
 
-var camera_x_rot = 0.0
+var cam_v_max = 60
+var cam_v_min = -50
 
-onready var camera_base = $"."
-onready var camera_rot = $camRot
-onready var camera_camera = $camRot/camera
+var rotation_sensitivity = 0.1
+var rotation_acceleration = 20
 
-func _init():
-	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+var yaw = 0
+var pitch = 0
+
+const JOY_SENSITIVITY = 4.5
 
 func _input(event):
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	if event is InputEventMouseMotion:
-		var camera_speed_this_frame = CAMERA_MOUSE_ROTATION_SPEED
-		rotate_camera(event.relative * camera_speed_this_frame)
+		camrot_h += -event.relative.x * rotation_sensitivity
+		camrot_v += event.relative.y * rotation_sensitivity
 
-func _physics_process(delta):
-	var rot = camera_rot.global_transform.basis.get_euler().y
-	
-		#camera physics
-	var camera_move = Vector2(
-		Input.get_action_strength("view_right") - Input.get_action_strength("view_left"),
-		Input.get_action_strength("view_down") - Input.get_action_strength("view_up"))
-	var camera_speed_this_frame = delta * CAMERA_CONTROLLER_ROTATION_SPEED
-	rotate_camera(camera_move * camera_speed_this_frame)
-	
-	var camera_basis = camera_rot.global_transform.basis
-	var camera_z = camera_basis.z
-	var camera_x = camera_basis.x
+func _process(delta):
+	camrot_v = clamp(camrot_v, cam_v_min, cam_v_max)
 
-	camera_z.y = 0
-	camera_z = camera_z.normalized()
-	camera_x.y = 0
-	camera_x = camera_x.normalized()
+	$camRot.rotation_degrees.y = lerp($camRot.rotation_degrees.y, camrot_h, delta * rotation_acceleration)
+	$camRot.rotation_degrees.x = lerp($camRot.rotation_degrees.x, camrot_v, delta * rotation_acceleration)
 
-func rotate_camera(move):
-	camera_base.rotate_y(-move.x)
-	# After relative transforms, camera needs to be renormalized.
-	camera_base.orthonormalize()
-	camera_x_rot += move.y
-	camera_x_rot = clamp(camera_x_rot, deg2rad(CAMERA_X_ROT_MIN), deg2rad(CAMERA_X_ROT_MAX))
-	camera_rot.rotation.x = camera_x_rot
